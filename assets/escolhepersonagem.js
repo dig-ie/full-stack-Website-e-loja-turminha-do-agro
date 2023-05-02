5041be9b718026ffc397cf8a78f23b5f594d6acd
Diêgo de Barros <debarrosdiego415@gmail.com>
2023-05-01 22:46:35 -0300

    salvando

--- assets/escolhepersonagem.js
+++ assets/escolhepersonagem.js
@@ -1,14 +1,49 @@
 var principal = document.getElementsById("section2")[0]; // div principal
 var lista = principal.getElementsByTagName("button"); // pega os itens da lista
 
-document.getElementById('btn1').addEventListener('click', function() {
-    principal.insertBefore( lista[(lista.length - 1)],lista[0] ); // move a última para antes da primeira 
-});
+//AINDA NÃO ESTÁ FUNCIONANDO - TÔ NO PROCESSO ------------------
+//AINDA NÃO ESTÁ FUNCIONANDO - TÔ NO PROCESSO ------------------
+function alternarCacá (){
 
-document.getElementById('btn2').addEventListener('click', function() {
-       
-    insertAfter( lista[0],lista[(lista.length - 1)] ); // move a primeira para depois da última
-});
+    getElementsById(cacá).style.display="default";
+    getElementsById(flora).style.display="none";
+    getElementsById(zeca).style.display="none";
+    getElementsById(vanvan).style.display="none";
+    getElementsById(neco).style.display="none";
+}
 
+function alternarFlora (){
 
+    getElementsById(cacá).style.display="none";
+    getElementsById(flora).style.display="default";
+    getElementsById(zeca).style.display="none";
+    getElementsById(vanvan).style.display="none";
+    getElementsById(neco).style.display="none";
+}
 
+function alternarZeca (){
+
+    getElementsById(cacá).style.display="none";
+    getElementsById(flora).style.display="none";
+    getElementsById(zeca).style.display="default";
+    getElementsById(vanvan).style.display="none";
+    getElementsById(neco).style.display="none";
+}
+
+function alternarVanvan (){
+
+    getElementsById(cacá).style.display="none";
+    getElementsById(flora).style.display="none";
+    getElementsById(zeca).style.display="none";
+    getElementsById(vanvan).style.display="default";
+    getElementsById(neco).style.display="none";
+}
+
+function alternarNeco (){
+
+    getElementsById(cacá).style.display="none";
+    getElementsById(flora).style.display="none";
+    getElementsById(zeca).style.display="none";
+    getElementsById(vanvan).style.display="none";
+    getElementsById(neco).style.display="default";
+}
