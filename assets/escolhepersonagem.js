2dcd9900710a452dd55c8418ff05ae0174915061
Diêgo <debarrosdiego415@gmail.com>
2023-05-02 21:30:21 -0300

    commit

--- assets/escolhepersonagem.js
+++ assets/escolhepersonagem.js
@@ -1,49 +1,9 @@
-var principal = document.getElementsById("section2")[0]; // div principal
-var lista = principal.getElementsByTagName("button"); // pega os itens da lista
+var listaPersonagens = document.querySelectorAll("#section1 img");
 
-//AINDA NÃO ESTÁ FUNCIONANDO - TÔ NO PROCESSO ------------------
-//AINDA NÃO ESTÁ FUNCIONANDO - TÔ NO PROCESSO ------------------
-function alternarCacá (){
+console.log(listaPersonagens[3].src);
 
-    getElementsById(cacá).style.display="default";
-    getElementsById(flora).style.display="none";
-    getElementsById(zeca).style.display="none";
-    getElementsById(vanvan).style.display="none";
-    getElementsById(neco).style.display="none";
-}
 
-function alternarFlora (){
+function alternaPersonagem (personagem){
 
-    getElementsById(cacá).style.display="none";
-    getElementsById(flora).style.display="default";
-    getElementsById(zeca).style.display="none";
-    getElementsById(vanvan).style.display="none";
-    getElementsById(neco).style.display="none";
-}
 
-function alternarZeca (){
-
-    getElementsById(cacá).style.display="none";
-    getElementsById(flora).style.display="none";
-    getElementsById(zeca).style.display="default";
-    getElementsById(vanvan).style.display="none";
-    getElementsById(neco).style.display="none";
-}
-
-function alternarVanvan (){
-
-    getElementsById(cacá).style.display="none";
-    getElementsById(flora).style.display="none";
-    getElementsById(zeca).style.display="none";
-    getElementsById(vanvan).style.display="default";
-    getElementsById(neco).style.display="none";
-}
-
-function alternarNeco (){
-
-    getElementsById(cacá).style.display="none";
-    getElementsById(flora).style.display="none";
-    getElementsById(zeca).style.display="none";
-    getElementsById(vanvan).style.display="none";
-    getElementsById(neco).style.display="default";
 }
